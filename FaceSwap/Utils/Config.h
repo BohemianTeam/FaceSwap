4b295e4651ea34daf64843f8b914a61139cacb29
Hung <trinhduchung266@gmail.com>
2012-05-29 16:00:43 +0700

    add

--- FaceSwap/Utils/Config.h
+++ FaceSwap/Utils/Config.h
@@ -11,6 +11,6 @@
 
 #define kFaceSwapFreeVersion    @"Free Version"
 #define kFaceSwapProVersion     @"Pro Version"
-#define kFaceSwapVersion        kFaceSwapFreeVersion
+#define kFaceSwapVersion        kFaceSwapProVersion
 
 #endif
